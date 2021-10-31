5669f0415813de1b2fc63c9ae97070fd49373bfe
tutuchaoren22 <38755786+tutuchaoren22@users.noreply.github.com>
2021-10-31 17:46:01 +0800

    [yuqian] fix: finish todo

--- chapter-one/homework/practise.js
+++ chapter-one/homework/practise.js
@@ -1,23 +1,29 @@
 //问题1
 const removeRepeat = () => {
     const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
-    // todo
-    // 输出[1, 2, 3, "a", "b"]
+    const a = [...new Set(arr)]
+    console.log(a);
 }
 removeRepeat();
 
 //问题2
-const findRepeat= () => {
+const findRepeat = () => {
     const array = [1, 2, 2, 3, 3, 3, 'a', 'a', 'b'];
-    // todo
-    // 输出[2, 3, "a"]
+    const arraySorted = array.sort();
+    let repeat = []
+    for (let i = 0; i < arraySorted.length; i++) {
+        if (arraySorted[i] === arraySorted[i + 1]) {
+            repeat.push(arraySorted[i])
+        }
+    }
+    console.log([...new Set(repeat)])
 }
 findRepeat();
 
 
 //问题3
-const clone= () => {
-    //todo
+const clone = () => {
+    return JSON.parse(JSON.stringify(obj));
 }
 const obj = {
     arr: [{a: 1, f: {q: 1}}, 2, 3],
@@ -29,14 +35,14 @@ console.log(obj1.string === obj.string);
 
 
 //问题4
-const question4= () => {
-    for (var i = 0; i < 5; i++) {
-        setTimeout(function() {
+const question4 = () => {
+    var j;
+    for (let i = 0; i < 5; i++) {
+        setTimeout(function () {
             console.log(i);
         }, 1000);
+        j = i + 1;
     }
-    console.log(i)
-    // todo
-    //修改代码输出 5,0,1,2,3,4
+    console.log(j);
 }
 question4();
